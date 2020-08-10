e9710b885a47fdf28130c266b2452fa8681a76c2
Saravanakumar GN <saravanakumargn@gmail.com>
2020-08-10 13:05:55 +0530

    initial commit

---
+++ components/Footer.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import { A, P, colors, darkColors } from '../common/styleguide';
+import ContentContainer from '../components/ContentContainer';
+import CustomAppearanceContext from '../context/CustomAppearanceContext';
+
+export default function Footer() {
+  return (
+    <CustomAppearanceContext.Consumer>
+      {context => (
+        <View
+          style={[
+            styles.container,
+            {
+              borderTopColor: context.isDark ? darkColors.border : colors.gray2,
+            },
+          ]}>
+          <ContentContainer>
+            <P style={styles.footerText}>
+              Missing a library?{' '}
+              <A href="https://github.com/react-native-community/react-native-directory#how-do-i-add-a-library">
+                Add it to the directory
+              </A>
+              . Want to learn more about React Native? Check out the{' '}
+              <A href="https://facebook.github.io/react-native/docs/getting-started.html">
+                official React Native docs
+              </A>
+              , and <A href="https://expo.io">Expo</A>.
+            </P>
+          </ContentContainer>
+        </View>
+      )}
+    </CustomAppearanceContext.Consumer>
+  );
+}
+
+let styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    padding: 24,
+    paddingLeft: 20,
+    marginBottom: 10,
+    borderTopWidth: 1,
+    marginTop: 10,
+  },
+  footerText: {
+    textAlign: 'center',
+    lineHeight: 22,
+    fontSize: 13,
+  },
+});
